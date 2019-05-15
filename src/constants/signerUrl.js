da67b5adde949ecaba2cd76c2e1ef528f2608634
Subir <schowdhuri@users.noreply.github.com>
2019-05-16 02:30:58 +0530

    Fix: Signer URL

--- src/constants/signerUrl.js
+++ src/constants/signerUrl.js
@@ -1,6 +1,6 @@
 const getUrl = () => process.env.REACT_APP_TARGET_ENV === "production"
   ? "https://gn80ai4ca2.execute-api.us-east-1.amazonaws.com/prod/"
-  : process.env.REACT_APP_TARGET_ENV === "state"
+  : process.env.REACT_APP_TARGET_ENV === "stage"
     ? "https://gn80ai4ca2.execute-api.us-east-1.amazonaws.com/prod/"
     : "http://localhost:3001/"
 
